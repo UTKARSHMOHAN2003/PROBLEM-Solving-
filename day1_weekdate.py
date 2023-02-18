678cdec9073c74845ad96069022444b337a0facc
UTKARSH MOHAN <115492964+UTKARSHMOHAN2003@users.noreply.github.com>
2023-02-18 21:13:42 +0530

    Delete day1_weekdate.py

--- day1_weekdate.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import calendar
-
-def findDay(date):
-	born = datetime.datetime.strptime(date, '%d %m %Y').weekday()
-	return (calendar.day_name[born])
-
-date = input()
-print(findDay(date))
