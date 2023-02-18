9a5e8f245c479c9193d0ab53e6cc78a1be458e36
UTKARSH MOHAN <115492964+UTKARSHMOHAN2003@users.noreply.github.com>
2023-02-18 21:15:18 +0530

    Delete day2_8.py

--- day2_8.py
+++
@@ -1,9 +0,0 @@
-j=4
-for i in range(1,5):
-    print(j*" ",end="")
-    for k in range(i,i+i):
-        print('*',end="")
-    for l in range(2,i+1):
-        print('*',end='')
-    j=j-1
-    print(" ")
